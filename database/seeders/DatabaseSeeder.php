6ba45a2e85c3d9c07cc309afda31e17be3a45a82
yonathandelfian10 <delvianyonathan@gmail.com>
2025-12-17 08:37:59 +0700

    chore: setup database seeder for default admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User; // Import Model User
+use Illuminate\Support\Facades\Hash; // Import Hashing untuk password
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Membuat Akun Admin Filament
+        User::create([
+            'name' => 'Yonathan Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'), // Password: password
+            'phone_number' => '081234567890',
+            'email_verified_at' => now(),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->command->info('User Admin berhasil dibuat! Email: admin@admin.com, Pass: password');
     }
 }
