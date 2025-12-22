3755295a81ece39d5829771dda954c9db7e0dbaa
yonathandelfian10 <delvianyonathan@gmail.com>
2025-12-22 15:59:33 +0700

    changes brand name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             // --- TAMBAHKAN BARIS INI ---
-            ->brandName('CMS Blog Saya')
+            ->brandName('LevelUp Blog')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
